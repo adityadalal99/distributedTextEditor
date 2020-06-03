2f89f00651bb6c76e98e1caa52c8f255c5afa50a
vastvik123 <54481347+vastvik123@users.noreply.github.com>
2020-06-03 22:13:56 +0530

    Basic functionalities of Editor

---
+++ src/texteditor/TextEditor.java
@@ -0,0 +1,43 @@
+package texteditor;
+
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class TextEditor{
+        
+    public void windowClosing(WindowEvent e){
+          int result = JOptionPane.showConfirmDialog(null, "Are you sure?","confirm",JOptionPane.YES_NO_OPTION,JOptionPane.QUESTION_MESSAGE);
+
+          if(result == JOptionPane.YES_OPTION){
+                  System.exit(0);
+          }else{
+                  //Do nothing
+          }
+    }
+
+    public static void main(String[] args) {
+        
+        textEditorGUI obj = new textEditorGUI();
+        obj.setBounds(0,0,700,700);
+        obj.setTitle("Shared Editor");
+        obj.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+        obj.setVisible(true);
+        obj.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        
+        obj.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent windowEvent){
+                if(obj.canExit()){
+                    System.exit(0);
+                }
+                else{
+                    obj.showDialogBox();
+                }
+            }
+        });
+   
+    }
+    
+}
